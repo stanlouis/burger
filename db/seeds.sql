ce62db63f31e4d05ca5379c43e81c605de87d04c
Stanley Louis <stanlouis@msn.com>
2018-06-27 17:19:59 -0400

    Initialize repository

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('White Castle Slider');
+INSERT INTO burgers (burger_name) VALUES ('In-N-Out Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('21 Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger King Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gardenburger', true);
+INSERT INTO burgers (burger_name) VALUES ('ShackBurger');
