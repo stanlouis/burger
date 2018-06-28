06c3ca7cd297ed6d7253b08a23f1d2855546011f
Stanley Louis <stanlouis@msn.com>
2018-06-28 14:09:31 -0400

    fix typo

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 require("dotenv").config();
 
 var connection;
-if (process.env.JAWSDB.URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({host: "localhost", port: 3306, user: "root", password: process.env.DB_PASS, database: "burgers_db"});
